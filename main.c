1386bf91aad4dc8163dbeba4395ee4025c65fa49
Robin Southern <betajaen@users.noreply.github.com>
2016-08-13 16:30:39 +0100

    Level pushing and popping mechanics. (restarting levels with new seeds)

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #define RETRO_WINDOW_CAPTION "Cats"
+#define RETRO_ARENA_SIZE Kilobytes(8)
 #define RETRO_WINDOW_DEFAULT_WIDTH 704
 #define RETRO_WINDOW_DEFAULT_HEIGHT 480
 #define RETRO_CANVAS_DEFAULT_WIDTH (RETRO_WINDOW_DEFAULT_WIDTH / 2)
@@ -23,6 +24,13 @@ static Animation      ANIMATEDSPRITE_QUOTE_WALK;
 static Sound          SOUND_COIN;
 static Bitmap         TILES1;
 
+void Splat_Tile(Bitmap* bitmap, S32 x, S32 y, S32 w, S32 h, U32 index)
+{
+  //U32 ox = bitmap->w % x;
+  //U32 oy = bitmap->h % y;
+}
+
+
 typedef enum 
 {
   AC_JUMP,
@@ -30,8 +38,8 @@ typedef enum
   AC_PAUSE,
   AC_CONFIRM,
   AC_CANCEL,
+  AC_RESET,
 } Actions;
-
 
 typedef enum
 {
@@ -68,10 +76,25 @@ typedef struct
 
   AnimatedSpriteObject player;
   Point velocity;
-} GameState;
+} Game;
 
+Game*      game;
+Level*     level;
 
-GameState* state;
+void PushLevel(int seed)
+{
+  Scope_Push('LEVL');
+  level = Scope_New(Level);
+  memset(level, 0, sizeof(Level));
+}
+
+void PopLevel()
+{
+  
+  
+  Scope_Pop();
+  level = NULL;
+}
 
 void Init(Settings* settings)
 {
@@ -80,6 +103,7 @@ void Init(Settings* settings)
 
   Input_BindKey(SDL_SCANCODE_SPACE, AC_JUMP);
   Input_BindKey(SDL_SCANCODE_LSHIFT, AC_RAGDOLL);
+  Input_BindKey(SDL_SCANCODE_R, AC_RESET);
 
   Font_Load("NeoSans.png", &FONT_NEOSANS, Colour_Make(0,0,255), Colour_Make(255,0,255));
   Bitmap_Load("cave.png", &SPRITESHEET, 0);
@@ -94,19 +118,32 @@ void Init(Settings* settings)
 
 void Start()
 {
-  state = Scope_New(GameState);
-
-  AnimatedSpriteObject_Make(&state->player, &ANIMATEDSPRITE_QUOTE_WALK, Canvas_GetWidth() / 2, Canvas_GetHeight() / 2);
-  AnimatedSpriteObject_PlayAnimation(&state->player, true, true);
-
-  state->velocity.x = 0;
-  state->velocity.y = 0;
+  game = Scope_New(Game);
+  //AnimatedSpriteObject_Make(&game->player, &ANIMATEDSPRITE_QUOTE_WALK, Canvas_GetWidth() / 2, Canvas_GetHeight() / 2);
+  //AnimatedSpriteObject_PlayAnimation(&game->player, true, true);
 
   // Music_Play("origin.mod");
+
+
 }
 
 void Step()
 {
-  Canvas_Splat(&TILES1, 0, 0, NULL);
+  if (Input_GetActionReleased(AC_RESET))
+  {
+    PopLevel();
+  }
+
+
+  if (level == NULL)
+  {
+    game->seed = game->seed << 8 | game->seed;
+    game->seed += 0x9328234;
+    PushLevel(game->seed);
+  }
+  
+  Canvas_PrintF(0, 0, &FONT_NEOSANS, 15, "%i", game->seed);
+
+  //Canvas_Splat(&TILES1, 0, 0, NULL);
   Canvas_Debug(&FONT_NEOSANS);
 }
